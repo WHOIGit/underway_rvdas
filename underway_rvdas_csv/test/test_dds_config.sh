773affa8bc01e54b9ae8da85c749f6015837e38f
BenAtWHOI <benjamin.foley@whoi.edu>
2024-10-03 10:06:56 -0400

    Testing out cached data server

--- underway_rvdas_csv/test/test_dds_config.sh
+++ underway_rvdas_csv/test/test_dds_config.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Simulate data
-cd /opt/openrvdas/logger/utils && python3 simulate_data.py --config ./data_sim_config.yaml &
+# Activate virtual environment
+source /opt/openrvdas/venv/bin/activate
 
-# 
+# Simulate data
+cd /opt/openrvdas/logger/utils && python3 simulate_data.py --config /home/befo/Desktop/WHOI/underway/underway_utils/data_sim_config.yaml &
+
+# Run logger manager
+cd /opt/openrvdas/server && python3 logger_manager.py --config /home/befo/Desktop/WHOI/underway/underway_rvdas/underway_rvdas_csv/conf/dds.yaml --start_data_server -v

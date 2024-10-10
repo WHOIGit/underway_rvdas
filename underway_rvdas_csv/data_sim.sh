e432e8534a5577d334655364db6ed988035dfc2b
BenAtWHOI <benjamin.foley@whoi.edu>
2024-10-10 14:26:19 -0400

    Deleted data sim script (wrong file)

--- underway_rvdas_csv/data_sim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE_PATH=/home/befo/Desktop/WHOI/underway/underway_utils/data_sim_config.yaml
-
-# Activate virtual environment
-source /opt/openrvdas/venv/bin/activate
-
-# Simulate data
-cd /opt/openrvdas/logger/utils/simulate_data.py --config $CONFIG_FILE_PATH &
